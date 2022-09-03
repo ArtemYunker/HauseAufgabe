08e70d2fed610e8459250cb30f33933f4cf495b3
Artem Yunker <art-yunker@yandex.ru>
2022-09-03 19:36:34 +0200

    task13

---
+++ Task13/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает,
+//  что третьей цифры нет.
+
+int number = new Random().Next();
+Console.WriteLine(number);
+
+if (number < 100)
+{
+    Console.WriteLine("Третьего числа нет");
+}
+else if (number < 1000)
+{
+    Console.WriteLine(number % 10);
+}
+else
+{
+    string str = number.ToString();
+    Console.WriteLine(number);
+    Console.WriteLine(str[2]);
+}
+
